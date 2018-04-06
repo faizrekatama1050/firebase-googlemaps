10cc18bc6cba3d8d4c092fbc1ac245dc8b985dcd
faizrekatama <faizrekatama@gmail.com>
2018-04-06 20:45:07 +0700

    Add map

--- main.js
+++ main.js
@@ -6,36 +6,69 @@ var config = {
     projectId: "belajar-map-a9aea",
     storageBucket: "belajar-map-a9aea.appspot.com",
     messagingSenderId: "854504402721"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var db    = firebase.database();
-  var ref   = db.ref('markers');
+var db = firebase.database();
+var ref = db.ref('markers');
+var map = null;
 
-  ref.on('value', getData, showError);
+ref.on('value', getData, showError);
 
-  function getData(data){
+function getData(data) {
     var marker = data.val();
     kunci = Object.keys(data.val());
-    console.log(marker);
     var content = "";
-    for(var i=0; i<kunci.length; i++){
-        content += marker[kunci[i]].coordinate.lat + "<br>";
+    for (var i = 0; i < kunci.length; i++) {
+        // content += marker[kunci[i]].coordinate.lat + "<br>";
+        var position = { lat: marker[kunci[i]].coordinate.lat, lng: marker[kunci[i]].coordinate.lng };
+        createMarker(position, null, marker[kunci[i]].info).setMap(map);
     }
-    document.getElementById('info').innerHTML = content;
-  }
-  function showError(err){
-    document.getElementById('info').innerHTML = err;
-  }
+    // document.getElementById('info').innerHTML = content;
+}
+function showError(err) {
+    // document.getElementById('info').innerHTML = err;
+}
 
-  document.getElementById('simpan').addEventListener('click', function(event){
+function initMap() {
+    var center = { lat: -5.351645650506815, lng: 105.40080353027338 };
+    var options = {
+        center: center,
+        zoom: 10
+    };
+    map = new google.maps.Map(document.getElementById('maps'), options);
+    createMarker(center).setMap(map);
+}
+
+function createMarker(position, iconImg = null, info = null) {
+    var marker = new google.maps.Marker(
+        {
+            position: position,
+            icon: iconImg
+        }
+    );
+    if (info) {
+        var infowindow = new google.maps.InfoWindow(
+            {
+                content: info
+            }
+        );
+
+        marker.addListener('click', function (e) {
+            infowindow.open(map, marker);
+        });
+    }
+    return marker;
+}
+
+document.getElementById('simpan').addEventListener('click', function (event) {
     ref.push(
         {
             coordinate: {
-                lat: document.getElementById('latitude').value,
-                lng: document.getElementById('longitude').value
+                lat: parseFloat(document.getElementById('latitude').value),
+                lng: parseFloat(document.getElementById('longitude').value)
             },
             info: document.getElementById('informasi').value
         }
     );
-  });
+});
